4030c367e0a68c4badb695f08d7b7763f2a787e4
vadxq <dxl@vadxq.com>
2023-07-10 18:43:20 +0800

    feat: http

---
+++ lib/common/http/api.dart
@@ -0,0 +1,10 @@
+
+class Api {
+
+  static const String baseUrl = "https://api.vadxq.com/";
+
+  static const String testUrl = "https://test-api.vadxq.com";
+
+  //登录
+  static const String login = "user/login";
+}
